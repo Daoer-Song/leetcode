8dcb8e556ea92dab95aeb574473c8cd641be1825
Daoer-Song <507370623@qq.com>
2021-03-01 15:11:43 +0800

    cpp 备注最优解

--- cpp/removeDuplicates.cpp
+++ cpp/removeDuplicates.cpp
@@ -36,3 +36,15 @@ int main()
     return 0;
 }
 
+// 利用vector的函数
+// unique是去重函数将相邻且重复的元素放到vector的尾部 注意 这里并没有删除元素 然后返回指向第一个重复元素的迭代器 再用erase函数擦除从这个元素到最后元素的所有的元素 之后返回数组长度 其实一行也可以实现，这不比博人传热血？
+
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//     //sort(nums.begin(), nums.end());
+//     nums.erase(unique(nums.begin(), nums.end()), nums.end());
+//     return nums.size();
+//     }
+// };
